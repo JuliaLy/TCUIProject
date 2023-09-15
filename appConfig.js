dc70232e700da504c59749da3f78a2eb07c679a2
Yulia Lysakova <Yulia.Lysakova@kofax.com>
2023-09-15 15:16:10 +0000

    Test

--- appConfig.js
+++ appConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     KTAVersion: "7.9.0.22.0.876",
     url: 'http://ws2019dc/totalagility',//'http://WIN-VDB1021B5BS/Totalagility',
     browser:'chromium', //'chromium' (for Chrome and Edge) or 'firefox' 
-    channel:'msedge', //'msedge' (for Edge browser) or 'chrome' (for Chrome browser); not used for Firefox browser
+    channel:'chrome', //'msedge' (for Edge browser) or 'chrome' (for Chrome browser); not used for Firefox browser
     isOPMT: false, 
     credentials: {
      username1:"ws2019dc\\Administrator",
